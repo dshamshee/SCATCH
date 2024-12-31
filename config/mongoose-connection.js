4bda1b7dd683d51e543366d9b02bb7beba3c4456
Danish <danishshamshee123@gmail.com>
2024-12-31 19:22:16 +0530

    create dynamic db connection and create development routes

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
+const config = require('config');
+const dbgr = require("debug")("development:mongoose");
 
 mongoose
-    .connect(`mongodb://127.0.0.1:27017/scatch`)
+    .connect(`${config.get("MONGODB_URI")}/scatch`)
     .then(function () {
-        console.log('Connected to MongoDB');
+        dbgr('Connected to MongoDB');
+        console.log("Connected")
     })
     .catch(function (err) {
+        dbgr(err);
         console.log(err);
     })
 
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const router = express.Router(); 
+const ownerModel = require('../models/owner-model');
+
+// These routes are only work when the project on development mode
+if(process.env.NODE_ENV === "development"){ // It checks if the project is in development mode or Production mode
+    router.post('/create', async function(req, res){
+
+      // It checks if any one Owner is already exist then don't create another Owner 
+      let owners = await  ownerModel.find();
+      if(owners.length > 0){
+        return res
+        .status(500)
+        .send("You don't have permission to create a new owner");
+      }
+
+      // If no any Owner is exist then create a new Owner
+      let {fullname, email, password} = req.body;
+      
+       let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password
+        })
+        res.status(201).send(createdOwner); 
+      
+    })
+}
+
+// _______________________________________________________________
+
 
 
 router.get('/', function(req, res){
     res.send("Hey it's working");
 })
+
+
 
 
 module.exports = router;
